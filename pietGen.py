53f6de005bc9c72b833930e7eb103b196d873520
noname13a <83711843+noname13a@users.noreply.github.com>
2023-05-30 08:41:39 +0200

    Add files via upload

---
+++ pietGen.py
@@ -0,0 +1,146 @@
+import numpy as np
+from PIL import Image
+
+class Color(object):
+
+	def __init__(self, color_table=None):
+		if color_table is None:
+			self.color_table = [1,0]
+		else:
+			self.color_table = color_table
+
+	def RGB(self):
+		if self.color_table[1] == 0:
+			#Red
+			if self.color_table[0] == 0:
+				#Light
+				return [255,192,192]
+			elif self.color_table[0] == 1:
+				#Normal
+				return [255,0,0]
+			elif self.color_table[0] == 2:
+				#Dark
+				return [192,0,0]
+		elif self.color_table[1] == 1:
+			#Yellow
+			if self.color_table[0] == 0:
+				#Light
+				return [255,255,192]
+			elif self.color_table[0] == 1:
+				#Normal
+				return [255,255,0]
+			elif self.color_table[0] == 2:
+				#Dark
+				return [192,192,0]
+		elif self.color_table[1] == 2:
+			#Green
+			if self.color_table[0] == 0:
+				#Light
+				return [192,255,192]
+			elif self.color_table[0] == 1:
+				#Normal
+				return [0,255,0]
+			elif self.color_table[0] == 2:
+				#Dark
+				return [0,192,0]
+		elif self.color_table[1] == 3:
+			#Cyan
+			if self.color_table[0] == 0:
+				#Light
+				return [192,255,255]
+			elif self.color_table[0] == 1:
+				#Normal
+				return [0,255,255]
+			elif self.color_table[0] == 2:
+				#Dark
+				return [0,192,192]
+		elif self.color_table[1] == 4:
+			#Blue
+			if self.color_table[0] == 0:
+				#Light
+				return [192,192,255]
+			elif self.color_table[0] == 1:
+				#Normal
+				return [0,0,255]
+			elif self.color_table[0] == 2:
+				#Dark
+				return [0,0,192]
+		elif self.color_table[1] == 5:
+			#Magenta
+			if self.color_table[0] == 0:
+				#Light
+				return [255,192,255]
+			elif self.color_table[0] == 1:
+				#Normal
+				return [255,0,255]
+			elif self.color_table[0] == 2:
+				#Dark
+				return [192,0,192]
+
+	def push_color(self):
+		self.color_table[0] = (self.color_table[0] + 1) % 3
+		return self.RGB()
+
+	def write_color(self):
+		self.color_table[0] = (self.color_table[0] + 2) % 3
+		self.color_table[1] = (self.color_table[1] + 5) % 6
+		return self.RGB()
+
+def draw_block(size,num):
+	block = np.zeros( (12,12,3), dtype=np.uint8 )
+
+	if num != 0:
+		old_push_color = current_color.push_color()
+		current_color.write_color()
+		block[:,:] = current_color.RGB()
+		block[0,0] = old_push_color
+		size = size +1
+	else:
+		block[:,:] = current_color.RGB()
+
+	pix_lft = 144-size
+	div = pix_lft // 12
+	rem = pix_lft % 12
+	if div !=0:
+		block[12-div:,]=0
+	block[11-div:,:rem]=0
+
+	pos_y = 12*num
+	pos_x = 0
+	piet[pos_x:pos_x+12,pos_y:pos_y+12] = block
+
+def draw_end(num):
+	block = np.zeros( (12,5,3), dtype=np.uint8 )
+
+	old_push_color = current_color.push_color()
+	block[:,:] = 255
+	block[0,0] = old_push_color
+	block[0,1] = current_color.write_color()
+
+	block[0:2,3] = 0
+	block[1,1] = 0
+	block[2,0] = 0
+	block[2,4] = 0
+	block[3,1:4] = 0
+	block[2,1:4]=current_color.write_color()
+
+	pos_y = 12*num
+	pos_x = 0
+	piet[pos_x:pos_x+12,pos_y:pos_y+5] = block
+
+
+current_color = Color()
+piet = []
+message = input("Introduce la flag: \n")
+painting_len = len(message)*12 + 5
+piet = np.zeros((12,painting_len,3), dtype=np.uint8)
+
+i = 0
+for char in message:
+	draw_block(ord(char),i)
+	i += 1
+draw_end(i)
+
+img = Image.fromarray(piet, "RGB")
+
+img.save("algo.png")
